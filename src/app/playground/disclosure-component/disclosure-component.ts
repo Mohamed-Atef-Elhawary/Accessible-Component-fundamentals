ff0f1d450df646e3d36790ef832efe8576976b92
mohamed atef <elhawaremohamed7@gmail.com>
2026-08-06 22:38:18 +0300

    chore: setup playground, and core folder structure

---
+++ src/app/playground/disclosure-component/disclosure-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disclosure-component',
+  imports: [],
+  templateUrl: './disclosure-component.html',
+  styleUrl: './disclosure-component.css',
+})
+export class DisclosureComponent {
+
+}
---
+++ src/app/playground/modal-dialog-component/modal-dialog-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-dialog-component',
+  imports: [],
+  templateUrl: './modal-dialog-component.html',
+  styleUrl: './modal-dialog-component.css',
+})
+export class ModalDialogComponent {
+
+}
